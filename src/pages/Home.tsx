5748045bb8e9a423394dacf91379a596f3b7ff9e
Elin Forsberg <47697397+Elin1043@users.noreply.github.com>
2023-08-27 14:02:41 +0200

    Image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,28 @@
-import { Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 type HomeProps = {};
 
 function Home(props: HomeProps) {
-  return <Typography variant="h1">Home</Typography>;
+  return (
+    <Box sx={{ height: "90vh", backgroundColor: "grey" }}>
+      <Grid container sx={{ backgroundColor: "red" }}>
+        <Typography variant="h2">
+          Elin Forsberg is a Game Developer and Software Engineer
+        </Typography>
+        <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+          }}
+          alt="The house from the offer."
+          src="HeadPic.jpg"
+        />
+      </Grid>
+    </Box>
+  );
 }
 
 export default Home;
