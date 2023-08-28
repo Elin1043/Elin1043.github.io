a50dfe19241264a371b3d6f2cf5dfb2fc8e0289b
Elin Forsberg <47697397+Elin1043@users.noreply.github.com>
2023-08-28 21:07:12 +0200

    Fixed download button for CV

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -1,16 +1,37 @@
 import { Document, Page, pdfjs } from "react-pdf";
-import samplePDF from "../assets/cv.pdf";
+import cvPDF from "../assets/cv.pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import { Button, Container, Typography, useTheme } from "@mui/material";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 type CVProps = {};
 
 function CV(props: CVProps) {
+  const theme = useTheme();
+  const onDownload = () => {
+    const link = document.createElement("a");
+    link.download = "Elin Forsberg CV";
+    link.href = cvPDF;
+    link.click();
+  };
   return (
-    <Document file={samplePDF}>
-      <Page pageNumber={1} />
-    </Document>
+    <Container sx={{ textAlign: "center" }}>
+      <Button
+        onClick={onDownload}
+        variant="contained"
+        sx={{
+          marginBottom: "10px",
+          color: "#FFF",
+          backgroundColor: (theme) => theme.palette.secondary.main,
+        }}
+      >
+        <Typography variant="body1">Download CV</Typography>
+      </Button>
+      <Document file={cvPDF}>
+        <Page pageNumber={1} />
+      </Document>
+    </Container>
   );
 }
 
