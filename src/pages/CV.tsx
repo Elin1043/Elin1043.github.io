015febf17aea7d878673c1a81ba5fc715ec8565b
Elin Forsberg <47697397+Elin1043@users.noreply.github.com>
2023-08-28 19:34:55 +0200

    Cv renders

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -1,9 +1,17 @@
-import { Typography } from "@mui/material";
+import { Document, Page, pdfjs } from "react-pdf";
+import samplePDF from "../assets/cv.pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 type CVProps = {};
 
 function CV(props: CVProps) {
-  return <Typography variant="h1">CV</Typography>;
+  return (
+    <Document file={samplePDF}>
+      <Page pageNumber={1} />
+    </Document>
+  );
 }
 
 export default CV;
