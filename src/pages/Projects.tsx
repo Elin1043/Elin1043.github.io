ef862dbf914591c37e1dfb06cb9ed5e761f15a15
Elin Forsberg <elin.forsberg@live.com>
2023-09-01 23:44:33 +0200

    Filled the project cards with mock information

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,21 +1,44 @@
-import { Card, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { HashTag } from "../components/HelperFunctions";
+import image from "../assets/kitty.jpg";
+import {
+  Theme,
+  createTheme,
+  responsiveFontSizes,
+  useTheme,
+} from "@mui/material/styles";
 
 type ProjectsProps = {};
 
 const Projects = (props: ProjectsProps) => {
   const projectList = ProjectList();
   const projects = projectList.map((projectItem) => (
-    <Grid item xs={4}>
+    <Grid item xs={4} sx={{ display: "flex", justifyContent: "center" }}>
       <ProjectCard project={projectItem}></ProjectCard>
     </Grid>
   ));
   return (
     <>
-      <Typography variant="h1">
-        <HashTag label={"projects"} textSize={32}></HashTag>
-      </Typography>
-      <Grid container spacing={2}>
+      <Grid container>
+        <Grid item xs={2}>
+          <HashTag label={"projects"} textSize={32}></HashTag>
+        </Grid>
+        <Grid item xs={8} sx={{ margin: "auto 0" }}>
+          <Divider
+            sx={{ borderColor: (theme) => theme.palette.secondary.main }}
+          ></Divider>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} sx={{ marginTop: "20px" }}>
         {projects}
       </Grid>
     </>
@@ -37,7 +60,44 @@ type ProjectCardProps = {
 
 const ProjectCard = (props: ProjectCardProps) => {
   const { project } = props;
-  return <Card sx={{ minHeight: "200px" }}>{project.projectName}</Card>;
+  return (
+    <Card
+      variant="outlined"
+      sx={{ minHeight: "300px", minWidth: "275px", borderColor: "#FFF" }}
+    >
+      <Box
+        component="img"
+        sx={{
+          maxHeight: { xs: 150, md: 150 },
+          width: "100%",
+          borderBottom: "1px solid",
+        }}
+        alt="Image of kitty"
+        src={project.image}
+      />
+      <Container sx={{ height: "30px", borderBottom: "1px solid" }}>
+        <Typography variant="body1">{project.skillsAndTools}</Typography>
+      </Container>
+      <Container>
+        <Typography variant="h6" fontWeight={700}>
+          {project.projectName}
+        </Typography>
+        <Typography variant="body1">{project.projectDescription}</Typography>
+        <Button
+          variant="outlined"
+          sx={{
+            margin: "10px 0",
+            textTransform: "initial",
+            borderColor: (theme) => theme.palette.secondary.main,
+          }}
+        >
+          <Typography variant="body1" fontWeight={700} color={"#FFF"}>
+            Demo
+          </Typography>
+        </Button>
+      </Container>
+    </Card>
+  );
 };
 
 type ProjectItem = {
@@ -50,11 +110,11 @@ type ProjectItem = {
 
 export const getProjectItems = (): ProjectItem => {
   let projectItem: ProjectItem = {
-    image: "string",
-    projectName: "string",
-    projectDescription: "string",
+    image: image,
+    projectName: "Portfolio",
+    projectDescription: "Hacked into google",
     projectLink: "string",
-    skillsAndTools: "string",
+    skillsAndTools: "React, Typescript, HTML, CSS",
   };
 
   return projectItem;
